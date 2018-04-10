896f449b5e9f57c9a77a97622a1989132b0fd0b2
Ahmad Jazuli <ahmadjazuli111@gmail.com>
2018-04-10 14:39:57 +0800

    Initial commit

---
+++ src/Pegawai.java
@@ -0,0 +1,12 @@
+public class Pegawai {
+	String nama,alamat,email;
+	double waktuKerja, waktuLembur, gajiPerJam, gajiLembur, gajiHasil;
+	
+	void cetakInfo() { 
+		System.out.println("============== [method tanpa nilai balik] ==================");
+		System.out.println("Nama Pegawai \t= "+nama);
+		System.out.println("Alamat \t \t= "+alamat);
+		System.out.println("Email \t \t= "+email);
+		System.out.println("============================================================");
+	}
+}
---
+++ src/PegawaiHitungGaji.java
@@ -0,0 +1,8 @@
+public class PegawaiHitungGaji {
+	public static void main(String[] args) {
+		Pegawai pg1 = new Pegawai();
+		pg1.nama = "Ahmad Jazuli";
+		
+		pg1.cetakInfo();
+	}
+}
