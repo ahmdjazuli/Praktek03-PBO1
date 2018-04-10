2dd0d9d014d5dc6323475ed4f499210b101cfdbe
Ahmad Jazuli <ahmadjazuli111@gmail.com>
2018-04-10 14:51:04 +0800

    Getter & Setter

--- src/Pegawai.java
+++ src/Pegawai.java
@@ -29,4 +29,69 @@ public class Pegawai {
 		gajiLembur = waktuLembur * gajiPerJam;
 		return gajiLembur;
 	}
+	
+	// Getter & Setter
+	public String getNama() {
+		return nama;
+	}
+
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+
+	public String getAlamat() {
+		return alamat;
+	}
+
+	public void setAlamat(String alamat) {
+		this.alamat = alamat;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public double getWaktuKerja() {
+		return waktuKerja;
+	}
+
+	public void setWaktuKerja(double waktuKerja) {
+		this.waktuKerja = waktuKerja;
+	}
+
+	public double getWaktuLembur() {
+		return waktuLembur;
+	}
+
+	public void setWaktuLembur(double waktuLembur) {
+		this.waktuLembur = waktuLembur;
+	}
+
+	public double getGajiPerJam() {
+		return gajiPerJam;
+	}
+
+	public void setGajiPerJam(double gajiPerJam) {
+		this.gajiPerJam = gajiPerJam;
+	}
+
+	public double getGajiLembur() {
+		return gajiLembur;
+	}
+
+	public void setGajiLembur(double gajiLembur) {
+		this.gajiLembur = gajiLembur;
+	}
+
+	public double getGajiHasil() {
+		return gajiHasil;
+	}
+
+	public void setGajiHasil(double gajiHasil) {
+		this.gajiHasil = gajiHasil;
+	}
 }
