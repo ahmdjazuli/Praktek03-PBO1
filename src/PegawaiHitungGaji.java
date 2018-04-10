e5fcc1f40e2db70c62e212e6bc2803f16ed657c6
Ahmad Jazuli <ahmadjazuli111@gmail.com>
2018-04-10 14:53:45 +0800

    pake method Set

--- src/PegawaiHitungGaji.java
+++ src/PegawaiHitungGaji.java
@@ -1,9 +1,9 @@
 public class PegawaiHitungGaji {
 	public static void main(String[] args) {
 		Pegawai pg1 = new Pegawai();
-		pg1.nama = "Ahmad Jazuli";
-		pg1.alamat = "Komplek Pangeran Antasari no.32 rt.3 rw.5";
-		pg1.email = "ahmadjazuli111@gmail.com";
+		pg1.setNama("Ahmad Jazuli");
+		pg1.setAlamat("Komplek Pangeran Antasari no.32 rt.3 rw.5");
+		pg1.setEmail("ahmadjazuli111@gmail.com");
 		
 		pg1.cetakInfo();
 		
