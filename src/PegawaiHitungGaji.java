3dfa8113923ac419f2520fb0427a5b4a41b94c55
Ahmad Jazuli <ahmadjazuli111@gmail.com>
2018-04-10 14:42:13 +0800

    bikin class aja dulu..
    tes..

--- src/PegawaiHitungGaji.java
+++ src/PegawaiHitungGaji.java
@@ -2,6 +2,8 @@ public class PegawaiHitungGaji {
 	public static void main(String[] args) {
 		Pegawai pg1 = new Pegawai();
 		pg1.nama = "Ahmad Jazuli";
+		pg1.alamat = "Komplek Pangeran Antasari no.32 rt.3 rw.5";
+		pg1.email = "ahmadjazuli111@gmail.com";
 		
 		pg1.cetakInfo();
 	}
