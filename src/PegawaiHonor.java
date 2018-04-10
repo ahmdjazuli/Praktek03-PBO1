e7a02cea83dd5faaadca07321a9cf4ee8668eeeb
Ahmad Jazuli <ahmadjazuli111@gmail.com>
2018-04-10 15:15:59 +0800

    Getter & Setter

--- src/PegawaiHonor.java
+++ src/PegawaiHonor.java
@@ -4,4 +4,12 @@ public class PegawaiHonor extends Pegawai {
 	void throttle() {
 		System.out.print("Tolong naikkan tunjangan saya :V");
 	}
+
+	public String getJenisHonor() {
+		return jenisHonor;
+	}
+
+	public void setJenisHonor(String jenisHonor) {
+		this.jenisHonor = jenisHonor;
+	}
 }
